4cf226c59662b1f0a6e734d2d21bf3a50ce61d77
Anton Okhrimenko <antony.okhrimenko@gmail.com>
2025-01-21 14:51:43 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import SpaceMission from "./homeworks/Homework06/SpaceMission";
+
+function App() {
+  return (
+    <>
+      {/* <Lesson06 /> */}
+      <SpaceMission />
+    </>
+  );
+}
+
+export default App;
---
+++ src/homeworks/Homework06/SpaceMission.tsx
@@ -0,0 +1,29 @@
+export default function SpaceMission() {
+  const shipName: string = "Apollo 11";
+  const crewCount: number = 3;
+  const missionStarted: boolean = true;
+  const crewNames: string[] = ["Нил Армстронг", "Майкл Коллинз", "Базз Олдрин"];
+  const captain: readonly [string, number] = ["Нил", 35];
+
+  function startMission(shipName: string): string {
+    return `Миссия ${shipName} началась!`;
+  }
+
+  return (
+    <div>
+      <h2>Название миссии: {shipName}</h2>
+      <p>Количество экипажа: {crewCount}</p>
+      <p>Миссия началась: {missionStarted ? "Да" : "Нет"}</p>
+      <p>
+        Капитан: {captain[0]}, возраст: {captain[1]}
+      </p>
+      <p>Экипаж:</p>
+      <ul>
+        {crewNames.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
+      </ul>
+      <p> {startMission(shipName)} </p>
+    </div>
+  );
+}
